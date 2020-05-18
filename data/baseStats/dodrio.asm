f942aed0b0ffc1a06ed737f1630f57e066404bb7
Zappa-92 <65498421+Zappa-92@users.noreply.github.com>
2020-05-17 21:29:23 -0300

    Update dodrio.asm

--- data/baseStats/dodrio.asm
+++ data/baseStats/dodrio.asm
@@ -4,7 +4,7 @@ db 110 ; base attack
 db 70 ; base defense
 db 100 ; base speed
 db 60 ; base special
-db NORMAL ; species type 1
+db FLYING ; species type 1
 db FLYING ; species type 2
 db 45 ; catch rate
 db 158 ; base exp yield
