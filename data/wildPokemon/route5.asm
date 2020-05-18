b4480d857415f8fc427237d7217a638a04f075ad
Zappa-92 <65498421+Zappa-92@users.noreply.github.com>
2020-05-18 08:55:26 -0300

    Update route5.asm

--- data/wildPokemon/route5.asm
+++ data/wildPokemon/route5.asm
@@ -5,11 +5,14 @@ Route5Mons:
 		db 13,PIDGEY
 		db 15,PIDGEY
 		db 10,MANKEY
+		db 10,MEOWTH
 		db 12,MANKEY
+		db 12,MEOWTH
 		db 15,ODDISH
 		db 16,ODDISH
 		db 16,PIDGEY
 		db 14,MANKEY
+		db 14,MEOWTH
 		db 16,MANKEY
 	ENDC
 	IF DEF(_BLUE)
@@ -17,11 +20,14 @@ Route5Mons:
 		db 13,PIDGEY
 		db 15,PIDGEY
 		db 10,MEOWTH
+		db 10,MANKEY
 		db 12,MEOWTH
+		db 12,MANKEY
 		db 15,BELLSPROUT
 		db 16,BELLSPROUT
 		db 16,PIDGEY
 		db 14,MEOWTH
+		db 14,MANKEY
 		db 16,MEOWTH
 	ENDC
 	db $00
