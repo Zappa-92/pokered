0ae366fb0989e7918eb7cba8a99f39c5072a8261
Zappa-92 <65498421+Zappa-92@users.noreply.github.com>
2020-05-23 18:44:27 -0300

    Update mansionb1.asm

--- data/wildPokemon/mansionb1.asm
+++ data/wildPokemon/mansionb1.asm
@@ -2,7 +2,6 @@ MansionMonsB1:
 	db $0A
 	IF DEF(_RED)
 		db 33,KOFFING
-		db 31,KOFFING
 		db 35,GROWLITHE
 		db 35,VULPIX
 		db 32,PONYTA
@@ -10,13 +9,11 @@ MansionMonsB1:
 		db 40,WEEZING
 		db 34,PONYTA
 		db 35,GRIMER
-		db 42,WEEZING
 		db 42,MUK
 		db 38,MAGMAR
 	ENDC
 	IF DEF(_BLUE)
 		db 33,GRIMER
-		db 31,GRIMER
 		db 35,VULPIX
 		db 35,GROWLITHE
 		db 32,PONYTA
@@ -24,7 +21,7 @@ MansionMonsB1:
 		db 40,MUK
 		db 34,PONYTA
 		db 35,KOFFING
-		db 38,MAGMAR
 		db 42,WEEZING
+		db 38,MAGMAR
 	ENDC
 	db $00
