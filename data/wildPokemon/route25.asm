66359e30d2ba45cd49bd326d3b7239f1c90b0f87
Zappa-92 <65498421+Zappa-92@users.noreply.github.com>
2020-05-24 19:11:13 -0300

    Update route25.asm

--- data/wildPokemon/route25.asm
+++ data/wildPokemon/route25.asm
@@ -1,27 +1,27 @@
 Route25Mons:
 	db $0F
 	IF DEF(_RED)
-		db 8,WEEDLE
-		db 9,KAKUNA
-		db 13,PIDGEY
-		db 12,ODDISH
-		db 13,ODDISH
-		db 12,ABRA
-		db 14,ODDISH
-		db 10,ABRA
-		db 7,METAPOD
-		db 8,CATERPIE
+		db 14,WEEDLE
+		db 15,KAKUNA
+		db 18,PIDGEY
+		db 17,ODDISH
+		db 18,ODDISH
+		db 13,ABRA
+		db 19,ODDISH
+		db 14,ABRA
+		db 13,METAPOD
+		db 14,CATERPIE
 	ENDC
 	IF DEF(_BLUE)
-		db 8,CATERPIE
-		db 9,METAPOD
-		db 13,PIDGEY
-		db 12,BELLSPROUT
-		db 13,BELLSPROUT
-		db 12,ABRA
-		db 14,BELLSPROUT
-		db 10,ABRA
-		db 7,KAKUNA
-		db 8,WEEDLE
+		db 14,CATERPIE
+		db 15,METAPOD
+		db 18,PIDGEY
+		db 17,BELLSPROUT
+		db 18,BELLSPROUT
+		db 13,ABRA
+		db 19,BELLSPROUT
+		db 14,ABRA
+		db 13,KAKUNA
+		db 14,WEEDLE
 	ENDC
 	db $00
