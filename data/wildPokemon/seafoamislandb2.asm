9431df51d526a694cefc933b564b609f4af4794d
Zappa-92 <65498421+Zappa-92@users.noreply.github.com>
2020-05-24 18:44:16 -0300

    Update seafoamislandb2.asm

--- data/wildPokemon/seafoamislandb2.asm
+++ data/wildPokemon/seafoamislandb2.asm
@@ -1,25 +1,25 @@
 IslandMonsB2:
 	db $0A
 	IF DEF(_RED)
-		db 32,SEEL
-		db 32,SLOWPOKE
-		db 28,PSYDUCK
-		db 28,KRABBY
-		db 30,STARYU
-		db 30,HORSEA
-		db 28,SHELLDER
-		db 37,SLOWBRO
-		db 30,JYNX
+		db 37,SEEL
+		db 37,SLOWPOKE
+		db 37,PSYDUCK
+		db 35,KRABBY
+		db 37,STARYU
+		db 36,HORSEA
+		db 34,SHELLDER
+		db 39,SLOWBRO
+		db 38,JYNX
 	ENDC
 	IF DEF(_BLUE)
-		db 32,SEEL
-		db 32,PSYDUCK
-		db 28,SLOWPOKE
-		db 28,HORSEA
-		db 30,SHELLDER
-		db 30,KRABBY
-		db 28,STARYU
-		db 37,GOLDUCK
-		db 30,JYNX
+		db 37,SEEL
+		db 37,PSYDUCK
+		db 37,SLOWPOKE
+		db 35,HORSEA
+		db 37,SHELLDER
+		db 36,KRABBY
+		db 34,STARYU
+		db 39,GOLDUCK
+		db 38,JYNX
 	ENDC
 	db $00
