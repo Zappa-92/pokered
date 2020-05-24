d65d04cc030127f18e5bb510b9c23ccd709e1589
Zappa-92 <65498421+Zappa-92@users.noreply.github.com>
2020-05-24 19:54:30 -0300

    Update pokemontower5.asm

--- data/wildPokemon/pokemontower5.asm
+++ data/wildPokemon/pokemontower5.asm
@@ -1,13 +1,13 @@
 TowerMons5:
 	db $0A
-	db 20,GASTLY
-	db 21,GASTLY
-	db 22,GASTLY
-	db 23,GASTLY
-	db 19,GASTLY
-	db 18,GASTLY
-	db 25,HAUNTER
-	db 20,CUBONE
-	db 22,CUBONE
-	db 24,GASTLY
+	db 26,GASTLY
+	db 27,GASTLY
+	db 28,GASTLY
+	db 29,GASTLY
+	db 30,GASTLY
+	db 25,GASTLY
+	db 33,HAUNTER
+	db 26,CUBONE
+	db 27,CUBONE
+	db 31,GASTLY
 	db $00
