22c98f4a45709ac638cbebfe765c8fc82b0aeadc
Zappa-92 <65498421+Zappa-92@users.noreply.github.com>
2020-05-24 19:19:37 -0300

    Update route22.asm

--- data/wildPokemon/route22.asm
+++ data/wildPokemon/route22.asm
@@ -1,27 +1,27 @@
 Route22Mons:
 	db $19
 	IF DEF(_RED)
-		db 3,RATTATA
-		db 3,NIDORAN_M
 		db 4,RATTATA
 		db 4,NIDORAN_M
-		db 2,RATTATA
-		db 2,MANKEY
-		db 3,SPEAROW
-		db 5,SPEAROW
-		db 3,NIDORAN_F
-		db 4,NIDORAN_F
+		db 5,RATTATA
+		db 5,NIDORAN_M
+		db 3,RATTATA
+		db 4,MANKEY
+		db 4,SPEAROW
+		db 6,SPEAROW
+		db 5,NIDORAN_F
+		db 6,NIDORAN_F
 	ENDC
 	IF DEF(_BLUE)
-		db 3,RATTATA
-		db 3,NIDORAN_F
 		db 4,RATTATA
 		db 4,NIDORAN_F
-		db 2,RATTATA
-		db 2,MANKEY
-		db 3,SPEAROW
-		db 5,SPEAROW
-		db 3,NIDORAN_M
-		db 4,NIDORAN_M
+		db 5,RATTATA
+		db 5,NIDORAN_F
+		db 3,RATTATA
+		db 4,MANKEY
+		db 4,SPEAROW
+		db 6,SPEAROW
+		db 5,NIDORAN_M
+		db 6,NIDORAN_M
 	ENDC
 	db $00
