4e71bd3b750d85db01922e1f578dc7b151a323dd
Zappa-92 <65498421+Zappa-92@users.noreply.github.com>
2020-05-24 11:55:43 -0300

    Update evos_moves.asm

--- data/evos_moves.asm
+++ data/evos_moves.asm
@@ -1505,7 +1505,7 @@ CaterpieEvosMoves:
 MetapodEvosMoves:
 ; Evolutions
 	db EV_LEVEL, 10, BUTTERFREE
-	db 0
+	db 7, HARDEN
 ; Learnset
 	db 0
 
@@ -1513,7 +1513,7 @@ ButterfreeEvosMoves:
 ; Evolutions
 	db 0
 ; Learnset
-	db 12, CONFUSION
+	db 10, CONFUSION
 	db 15, POISONPOWDER
 	db 16, STUN_SPORE
 	db 17, SLEEP_POWDER
