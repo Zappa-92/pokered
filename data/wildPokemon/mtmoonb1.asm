73f913fabeabc374ed3d517ad558abd93c619668
Zappa-92 <65498421+Zappa-92@users.noreply.github.com>
2020-05-24 19:57:23 -0300

    Update mtmoonb1.asm

--- data/wildPokemon/mtmoonb1.asm
+++ data/wildPokemon/mtmoonb1.asm
@@ -1,13 +1,13 @@
 MoonMonsB1:
 	db $0A
-	db 8,ZUBAT
-	db 7,ZUBAT
-	db 7,GEODUDE
-	db 8,GEODUDE
-	db 9,ZUBAT
-	db 10,PARAS
-	db 10,ZUBAT
+	db 12,ZUBAT
 	db 11,ZUBAT
-	db 9,CLEFAIRY
-	db 9,MACHOP
+	db 11,GEODUDE
+	db 12,GEODUDE
+	db 13,ZUBAT
+	db 13,PARAS
+	db 14,ZUBAT
+	db 15,ZUBAT
+	db 14,CLEFAIRY
+	db 14,MACHOP
 	db $00
