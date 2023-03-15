108efdc68a9ec6f45b74b7c4021e356f3e4a5955
Alexis Bastiani <65498421+Zappa-92@users.noreply.github.com>
2023-03-15 10:50:25 -0300

    Update abra.asm

--- data/baseStats/abra.asm
+++ data/baseStats/abra.asm
@@ -1,7 +1,7 @@
 db DEX_ABRA ; pokedex id
-db 25 ; base hp
-db 20 ; base attack
-db 15 ; base defense
+db 45 ; base hp
+db 25 ; base attack
+db 20 ; base defense
 db 90 ; base speed
 db 105 ; base special
 db PSYCHIC ; species type 1
