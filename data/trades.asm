89e825afeeee37e959f97e3880f6fcb7dbdc7c50
Alexis Bastiani <65498421+Zappa-92@users.noreply.github.com>
2023-10-05 19:13:41 -0300

    Update trades.asm

--- data/trades.asm
+++ data/trades.asm
@@ -1,12 +1,12 @@
 TradeMons:
 ; givemonster, getmonster, textstring, nickname (11 bytes), 14 bytes total
-	db CLEFABLE,  PSYDUCK,  0,"TERRY@@@@@@"
+	db CLEFAIRY,  PSYDUCK,  0,"TERRY@@@@@@"
 	db ABRA,      MR_MIME,  0,"MARCEL@@@@@"
 	db CHARMANDER,CHANSEY,  2,"CHIKUCHIKU@"
 	db GASTLY,    POLIWAG,  0,"SAILOR@@@@@"
 	db PARAS,     KRABBY,   2,"DUX@@@@@@@@"
 	db VULPIX,    LICKITUNG,0,"MARC@@@@@@@"
 	db GOLBAT,    JYNX,     1,"LOLA@@@@@@@"
-	db MACHOKE,  ELECTABUZZ,1,"DORIS@@@@@@"
+	db PRIMEAPE,  ELECTABUZZ,1,"DORIS@@@@@@"
 	db RHYHORN,   TANGELA,  2,"CRINKLES@@@"
 	db SANDSHREW, SLOWPOKE, 2,"SPOT@@@@@@@"
