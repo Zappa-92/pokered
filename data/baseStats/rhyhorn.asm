ddd8544211f8b64ff0190e0d3ad561218c8ecf0d
Alexis Bastiani <65498421+Zappa-92@users.noreply.github.com>
2023-10-05 15:39:58 -0300

    Update rhyhorn.asm

--- data/baseStats/rhyhorn.asm
+++ data/baseStats/rhyhorn.asm
@@ -12,8 +12,8 @@ INCBIN "pic/bmon/rhyhorn.pic",0,1 ; 77, sprite dimensions
 dw RhyhornPicFront
 dw RhyhornPicBack
 ; attacks known at lvl 0
-db HORN_ATTACK
-db 0
+db 0, ROCK_THROW
+db 0, GROWL
 db 0
 db 0
 db 5 ; growth rate
