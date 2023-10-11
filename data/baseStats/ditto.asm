3c431f24b83315ce4be58b78bdb308f130ee6221
Alexis Bastiani <65498421+Zappa-92@users.noreply.github.com>
2023-10-11 15:34:04 -0300

    Update ditto.asm

--- data/baseStats/ditto.asm
+++ data/baseStats/ditto.asm
@@ -1,9 +1,9 @@
 db DEX_DITTO ; pokedex id
-db 68 ; base hp
-db 68 ; base attack
-db 68 ; base defense
-db 68 ; base speed
-db 68 ; base special
+db 73 ; base hp
+db 73 ; base attack
+db 73 ; base defense
+db 73 ; base speed
+db 73 ; base special
 db NORMAL ; species type 1
 db NORMAL ; species type 2
 db 35 ; catch rate
