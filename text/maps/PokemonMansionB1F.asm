53a4fb60813769a0471d66851c3fc8b1492caeb3
Alexis Bastiani <65498421+Zappa-92@users.noreply.github.com>
2023-10-16 17:59:55 -0300

    Update PokemonMansionB1F.asm

--- text/maps/PokemonMansionB1F.asm
+++ text/maps/PokemonMansionB1F.asm
@@ -30,22 +30,26 @@ _Mansion4AfterBattleText2::
 
 _Mansion4Text7::
 	text "Diary; Sept. 1"
-	line "The project succeded."
-	cont "MEWTWO was created."
+	line "The project"
+	cont "succeded, MEWTWO"
+	cont "was born."
 
 	para "The combined DNA"
-	line "of human & #MON"
-	cont "have created a truly"
-	cont "intelligent and strong"
-	cont "MONSTER"
+	line "of human and"
+	cont "#MON, have"
+	cont "created a truly"
+	cont "intelligent and"
+	cont "strong MONSTER."
 	
 	para "We cannot control it,"
 	line "its psychic powers"
 	cont "are far too powerful."
 	
-	para "We contacted SILPH Co"
-	line "to start the production"
-	cont "of a new BALL prototype"
-	cont "it could be our only"
-	cont "chance to catch it."
+	para "We contacted SILPH"
+	line "to start production"
+	cont "of a new BALL"
+	cont "prototype."
+	cont "it could be our"
+	cont "only chance"
+	cont "to catch it."
 	done
