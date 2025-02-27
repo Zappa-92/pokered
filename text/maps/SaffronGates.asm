1d3b162f44f2ef7040c97ef71947905f8c7f95ef
Alexis Bastiani <65498421+Zappa-92@users.noreply.github.com>
2025-02-27 18:31:16 -0300

    Update SaffronGates.asm

--- text/maps/SaffronGates.asm
+++ text/maps/SaffronGates.asm
@@ -4,7 +4,8 @@ _SaffronGateText_1dfe7::
 	cont "though!"
 
 	para "Oh wait there,"
-	line "the road's closed."
+	line "the road's closed"
+	cont "trouble ahead!"
 	done
 
 _SaffronGateText_8aaa9::
@@ -34,4 +35,36 @@ _SaffronGateText_1dff1::
 _SaffronGateText_1dff6::
 	text "Hi, thanks for"
 	line "the cool drinks!"
+
+	para "You're nice"
+	line "and strong enough"
+	cont "so keep on!"
+	done
+
+_SaffronGateText_FirstDrinkNoBadge::
+	text ""
+
+	para "..."
+	line "Glug glug..."
+	cont "..."
+	cont "Gulp..."
+	cont "Ahh..refreshing"
+
+	para "But wait!
+	line "Team Rocket"
+	cont "has invaded"
+	cont "Saffron! Only"
+	cont "strong trainers"
+	cont "may pass!"
+	done
+
+_SaffronGateText_NoBadgeAfterDrink::  ; ID 6
+	text "Thanks for the"
+	line "drinks, but"
+	cont "Team Rocket has"
+	cont "invaded Saffron"
+
+	para "Only strong"
+	line "#MON trainers"
+	cont "may pass!"
 	done
