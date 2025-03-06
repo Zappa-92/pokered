1b160936631fcc9f7a0f1aee1ee675568a29da9c
Alexis Bastiani <65498421+Zappa-92@users.noreply.github.com>
2025-03-06 16:41:21 -0300

    Update CinnabarLabFossilRoom.asm

--- data/mapObjects/CinnabarLabFossilRoom.asm
+++ data/mapObjects/CinnabarLabFossilRoom.asm
@@ -10,6 +10,7 @@ CinnabarLabFossilRoom_Object:
 	db 2 ; objects
 	object SPRITE_OAK_AIDE, 5, 2, WALK, 2, 1 ; person
 	object SPRITE_OAK_AIDE, 7, 6, STAY, UP, 2 ; person
+	object SPRITE_FAT_BALD_GUY, 2, 3, STAY, RIGHT, 3, OPP_BLAINE, 1
 
 	; warp-to
 	warp_to 2, 7, CINNABAR_LAB_FOSSIL_ROOM_WIDTH ; CINNABAR_LAB
