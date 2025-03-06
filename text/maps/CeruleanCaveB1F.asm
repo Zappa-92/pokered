0cb043685b52b6ddf73cc9db3b06a58483430f98
Alexis Bastiani <65498421+Zappa-92@users.noreply.github.com>
2025-03-06 13:23:12 -0300

    Update CeruleanCaveB1F.asm

--- text/maps/CeruleanCaveB1F.asm
+++ text/maps/CeruleanCaveB1F.asm
@@ -1,8 +1,59 @@
 _MewtwoBattleText::
-	text "Mew!"
+	text "MEW!"
 
 	para "wait...no"
 	line "you are the"
 	cont "experiment."
 
-	para "You are Mewtwo!@@"
+	para "MEWTWO!@@"
+
+_CeruleanCaveGiovanniBattleText::
+    	text "GIOVANNI: I came"
+    	line "here for MEWTWO,"
+    	cont "but you’ve claimed"
+    	cont "it already!"
+
+    	para "Show me its power,"
+    	line "use MEWTWO against"
+    	cont "me, now!"
+    	done
+
+_CeruleanCaveGiovanniLoseText::
+    	text "GIOVANNI: Even"
+    	line "MEWTWO couldn’t"
+    	cont "stop me?!"
+	cont "Pathetic..."
+    	done
+
+_CeruleanCaveGiovanniWinText::
+    	text "GIOVANNI: MEWTWO’s"
+    	line "strength... You’ve"
+    	cont "mastered it well!"
+	
+	para "The bond of a"
+	line "#MON and a"
+	cont "fierce TRAINER"
+    	done
+
+_CeruleanCaveGiovanniPostText::    
+	text "GIOVANNI: So you’re"
+    	line "set to face MEW?"
+
+    	para "Find the scientist,"
+    	line "the only one that"
+	cont "encountered it."
+
+	para "Back then, he"
+    	line "lived in Cinnabar."
+	cont "And he lead the"
+	cont "MEWTWO project."
+    	done
+
+_CeruleanCaveGiovanniNoMewtwoText::
+    	text "GIOVANNI: I"
+    	line "will only battle"
+	cont "with MEWTWO"
+    	cont "by your side!"
+
+	para "I'll wait."
+    	done
