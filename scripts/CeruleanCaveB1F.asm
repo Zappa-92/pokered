1ed4bc1d5b782e2ec4da85c7735045ab00416be3
Alexis Bastiani <65498421+Zappa-92@users.noreply.github.com>
2025-03-08 14:11:30 -0300

    Update CeruleanCaveB1F.asm

--- scripts/CeruleanCaveB1F.asm
+++ scripts/CeruleanCaveB1F.asm
@@ -19,8 +19,19 @@ CeruleanCaveB1FGiovanniBattle:
     jp z, CeruleanCaveB1FResetScript
     CheckEvent EVENT_BEAT_MEWTWO
     jr z, CeruleanCaveB1FResetScript  ; Mewtwo battle just ended
-    ; If Mewtwo beaten, set Giovanni event
+    ; If Mewtwo beaten, set Giovanni event and fade out post-item
     SetEvent EVENT_BEAT_GIOVANNI_CAVE_REMATCH
+    CheckEvent EVENT_GOT_HIGGS_FOSSIL
+    jr nz, .reset  ; Already faded
+    ; Fade-out after giving Higgs Fossil
+    call GBFadeOutToBlack
+    ld a, HS_CERULEAN_CAVE_B1F_GIOVANNI  ; $D2
+    ld [wMissableObjectIndex], a
+    predef HideObject
+    call UpdateSprites
+    call Delay3
+    call GBFadeInFromBlack
+.reset
     xor a
     ld [wJoyIgnore], a
     ld [wCeruleanCaveB1FCurScript], a
@@ -93,12 +104,54 @@ MewtwoText:
     call PrintText
     jr .done
 .postGiovanni
-    ld hl, CeruleanCaveGiovanniPostText
+    CheckEvent EVENT_GOT_HIGGS_FOSSIL
+    jr nz, .silent
+    ld hl, CeruleanCaveGiovanniPostFadeText
     call PrintText
+    lb bc, HIGGS_FOSSIL, 1
+    call GiveItem
+    jr nc, .BagFull
+    ld hl, ReceivedHiggsFossilText
+    call PrintText
+    SetEvent EVENT_GOT_HIGGS_FOSSIL
+    jr .done
+.BagFull
+    ld hl, HiggsFossilNoRoomText
+    call PrintText
+    jr .done
+.silent
+    ; No text after fade
     jr .done
 .noGiovanni
     ; Silence if neither Mewtwo nor Giovanni
+.done
+    jp TextScriptEnd
+
+CeruleanCaveGiovanniText:
+    TX_ASM
+    CheckEvent EVENT_GOT_HIGGS_FOSSIL
+    jr nz, .done  ; Silent if gone
+    CheckEvent EVENT_BEAT_GIOVANNI_CAVE_REMATCH
+    jr z, .done   ; Silent pre-battle (handled by MewtwoText)
+    ld hl, CeruleanCaveGiovanniPostFadeText
+    call PrintText
+    lb bc, HIGGS_FOSSIL, 1
+    call GiveItem
+    jr nc, .BagFull
+    ld hl, ReceivedHiggsFossilText
+    call PrintText
+    SetEvent EVENT_GOT_HIGGS_FOSSIL
+    call GBFadeOutToBlack
+    ld a, HS_CERULEAN_CAVE_B1F_GIOVANNI  ; $D2
+    ld [wMissableObjectIndex], a
+    predef HideObject
+    call UpdateSprites
+    call Delay3
+    call GBFadeInFromBlack
     jr .done
+.BagFull
+    ld hl, HiggsFossilNoRoomText
+    call PrintText
 .done
     jp TextScriptEnd
 
@@ -128,4 +181,17 @@ CeruleanCaveGiovanniPostText:
 
 CeruleanCaveGiovanniNoMewtwoText:
     TX_FAR _CeruleanCaveGiovanniNoMewtwoText
+    db "@"
+
+CeruleanCaveGiovanniPostFadeText:
+    TX_FAR _CeruleanCaveGiovanniPostFadeText
+    db "@"
+
+ReceivedHiggsFossilText:
+    TX_FAR _ReceivedHiggsFossilText
+    TX_SFX_ITEM_1
+    db "@"
+
+HiggsFossilNoRoomText:
+    TX_FAR _HiggsFossilNoRoomText
     db "@"
