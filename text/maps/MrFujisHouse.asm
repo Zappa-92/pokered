77d43e4265431600309b14ab26a39c228116f171
Alexis Bastiani <65498421+Zappa-92@users.noreply.github.com>
2025-03-10 10:59:44 -0300

    Update MrFujisHouse.asm

--- text/maps/MrFujisHouse.asm
+++ text/maps/MrFujisHouse.asm
@@ -84,3 +84,48 @@ _LavenderHouse1Text6::
 	para "Gone! It's been"
 	line "clipped out!"
 	done
+
+_FujiAncientFluteText::
+    	text "MR. FUJI: I’ve"
+    	line "never seen a"
+    	cont "trainer like you"
+    	cont "that cares for all"
+	cont "#MON, and strong"
+    	cont "enough to contain"
+    	cont "MEWTWO."
+
+    	para "I want you to"
+	line "have this ANCIENT"
+    	cont "FLUTE." 
+	
+	para "I found it years"
+	line "ago, when looking"
+	cont "for traces of MEW"
+	
+   	para "Natives, deep in"
+	line "the jungle used"
+   	cont "it in many rituals"
+	cont "to call for MEW"
+    	cont "and celebrate"
+    	cont "life."
+
+    	para "Go to CINNABAR"
+    	line "MANSION, look for"
+    	cont "the MEW shrine."
+    	done
+
+_ReceivedAncientFluteText::
+    	text "Received an"
+    	line "ANCIENTFLUTE!@"
+
+_AncientFluteNoRoomText::
+    	text "No room for the"
+    	line "ANCIENTFLUTE!@"
+
+_FujiPostAncientText::
+    	text "MR. FUJI: The"
+    	line "shrine’s on the"
+    	cont "last floor. Good"
+    	cont "luck, <PLAYER>."
+    	done
+
