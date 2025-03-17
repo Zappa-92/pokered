14aa3a3fc6d56bb3de98b7ba9b0d93fed8b48959
Alexis Bastiani <65498421+Zappa-92@users.noreply.github.com>
2025-03-17 12:32:01 -0300

    Update electabuzz.asm

--- data/baseStats/electabuzz.asm
+++ data/baseStats/electabuzz.asm
@@ -12,9 +12,9 @@ INCBIN "pic/bmon/electabuzz.pic",0,1 ; 66, sprite dimensions
 dw ElectabuzzPicFront
 dw ElectabuzzPicBack
 ; attacks known at lvl 0
-db QUICK_ATTACK
-db LEER
 db THUNDERSHOCK
+db LEER
+db QUICK_ATTACK
 db 0
 db 0 ; growth rate
 ; learnset
