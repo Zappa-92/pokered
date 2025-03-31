8724331c4d6615cbc9a8375398d658f00cf79c63
Alexis Bastiani <65498421+Zappa-92@users.noreply.github.com>
2025-03-31 17:38:39 -0300

    Update init_battle_variables.asm

--- engine/battle/init_battle_variables.asm
+++ engine/battle/init_battle_variables.asm
@@ -1,38 +1,45 @@
 InitBattleVariables:
-	ld a, [hTilesetType]
-	ld [wSavedTilesetType], a
-	xor a
-	ld [wActionResultOrTookBattleTurn], a
-	ld [wBattleResult], a
-	ld hl, wPartyAndBillsPCSavedMenuItem
-	ld [hli], a
-	ld [hli], a
-	ld [hli], a
-	ld [hl], a
-	ld [wListScrollOffset], a
-	ld [wCriticalHitOrOHKO], a
-	ld [wBattleMonSpecies], a
-	ld [wPartyGainExpFlags], a
-	ld [wPlayerMonNumber], a
-	ld [wEscapedFromBattle], a
-	ld [wMapPalOffset], a
-	ld hl, wPlayerHPBarColor
-	ld [hli], a ; wPlayerHPBarColor
-	ld [hl], a ; wEnemyHPBarColor
-	ld hl, wCanEvolveFlags
-	ld b, $3c
+    ld a, [hTilesetType]
+    ld [wSavedTilesetType], a
+    xor a                      ; a = 0
+    ld [wActionResultOrTookBattleTurn], a
+    ld [wBattleResult], a
+    ld hl, wPartyAndBillsPCSavedMenuItem
+    ld [hli], a
+    ld [hli], a
+    ld [hli], a
+    ld [hl], a
+    ld [wListScrollOffset], a
+    ld [wCriticalHitOrOHKO], a
+    ld [wBattleMonSpecies], a
+    ld [wPartyGainExpFlags], a
+    ld [wPlayerMonNumber], a
+    ld [wEscapedFromBattle], a
+    ld [wMapPalOffset], a
+    ld hl, wPlayerHPBarColor
+    ld [hli], a                ; wPlayerHPBarColor
+    ld [hl], a                 ; wEnemyHPBarColor
+    ld hl, wCanEvolveFlags
+    ld b, $3c
 .loop
-	ld [hli], a
-	dec b
-	jr nz, .loop
-	inc a ; POUND
-	ld [wTestBattlePlayerSelectedMove], a
-	ld a, [wCurMap]
-	cp SAFARI_ZONE_EAST
-	jr c, .notSafariBattle
-	cp SAFARI_ZONE_CENTER_REST_HOUSE
-	jr nc, .notSafariBattle
-	ld a, BATTLE_TYPE_SAFARI
-	ld [wBattleType], a
+    ld [hli], a
+    dec b
+    jr nz, .loop
+    ; Clear new switch flags
+    ld [wPlayerSwitched], a    ; Added: Clear player Teleport switch flag
+    ld [wEnemySwitched], a     ; Added: Clear enemy Teleport switch flag
+    ld [wForceEnemyToSwitch], a ; Added: Clear Whirlwind/Roar enemy switch flag
+    ld [wForcePlayerToSwitch], a ; Added: Clear enemy Whirlwind/Roar player switch flag
+    inc a                      ; POUND
+    ld [wTestBattlePlayerSelectedMove], a
+    ld a, [wCurMap]
+    cp SAFARI_ZONE_EAST
+    jr c, .notSafariBattle
+    cp SAFARI_ZONE_CENTER_REST_HOUSE
+    jr nc, .notSafariBattle
+    ld a, BATTLE_TYPE_SAFARI
+    ld [wBattleType], a
 .notSafariBattle
-	jpab PlayBattleMusic
+    call UpdatePartyAlive      ; Added: Initialize wPartyAlive
+    call UpdateEnemyNumAlive   ; Added: Initialize wEnemyNumAlive
+    jpab PlayBattleMusic
