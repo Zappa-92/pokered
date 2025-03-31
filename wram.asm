60e1b365b03ac8118157f0a89a9c733e0581ba1d
Alexis Bastiani <65498421+Zappa-92@users.noreply.github.com>
2025-03-31 17:32:23 -0300

    Update wram.asm

--- wram.asm
+++ wram.asm
@@ -3197,6 +3197,12 @@ wBoxDataEnd::
 wTempDamage:: ds 2    ; $DEE3-$DEE4
 wTotalDamage:: ds 2   ; $DEE5-$DEE6
 wLeechSeedDamage:: ds 2 ; $DEE7-$DEE8
+wPartyAlive::         ds 1 ; $DEE9 - Number of living player Pokémon
+wEnemyNumAlive::      ds 1 ; $DEEA - Number of living enemy Pokémon
+wPlayerSwitched::     ds 1 ; $DEEB - Flag for player Teleport switch
+wEnemySwitched::      ds 1 ; $DEEC - Flag for enemy Teleport switch
+wForceEnemyToSwitch:: ds 1 ; $DEED - Flag for player Whirlwind/Roar
+wForcePlayerToSwitch:: ds 1 ; $DEEE - Flag for enemy Whirlwind/Roar
 
 SECTION "Stack", WRAM0
 wStack:: ; dfff
