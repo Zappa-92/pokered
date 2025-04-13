92c861548b6fc311bf9afa51626f9ce1d27d00c3
Alexis Bastiani <65498421+Zappa-92@users.noreply.github.com>
2025-04-13 11:28:37 -0300

    Update main.asm

--- main.asm
+++ main.asm
@@ -928,7 +928,6 @@ INCLUDE "engine/trade.asm"
 INCLUDE "engine/intro.asm"
 INCLUDE "engine/trade2.asm"
 INCLUDE "engine/battle/status_effects.asm"
-INCLUDE "engine/battle/status_effects.asm"
 INCLUDE "engine/overworld/pokemon_party.asm"
 
 SECTION "bank11", ROMX
