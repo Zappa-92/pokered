dd517588bec9425a2465cae33e8e3717299f9c17
Alexis Bastiani <65498421+Zappa-92@users.noreply.github.com>
2026-03-14 00:27:09 -0300

    Nuevo Fosil

--- constants/hide_show_constants.asm
+++ constants/hide_show_constants.asm
@@ -114,7 +114,7 @@ const_value = 0
 	const HS_MT_MOON_1F_ITEM_3             ; 69 X
 	const HS_MT_MOON_1F_ITEM_4             ; 6A X
 	const HS_MT_MOON_1F_ITEM_5             ; 6B X
-	const HS_MT_MOON_1F_ITEM_6             ; 6C X
+	const HS_ROCK_TUNNEL_B1F_FOSSIL        ; NUEVO FOSIL
 	const HS_MT_MOON_B2F_FOSSIL_1          ; 6D
 	const HS_MT_MOON_B2F_FOSSIL_2          ; 6E
 	const HS_MT_MOON_B2F_ITEM_1            ; 6F X
