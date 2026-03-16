11ed95cb7b60e7c9506bd79cfc27711a3348fb9f
Alexis Bastiani <65498421+Zappa-92@users.noreply.github.com>
2026-03-16 12:10:13 -0300

    Update CeruleanCaveB1F.asm

--- text/maps/CeruleanCaveB1F.asm
+++ text/maps/CeruleanCaveB1F.asm
@@ -1,8 +1,84 @@
 _MewtwoBattleText::
-	text "Mew!"
+	text "MEW!"
 
 	para "wait...no"
 	line "you are the"
 	cont "experiment."
 
-	para "You are Mewtwo!@@"
+	para "MEWTWO!@@"
+
+_CeruleanCaveGiovanniBattleText::
+    	text "GIOVANNI: I came"
+    	line "here for MEWTWO,"
+    	cont "but you’ve claimed"
+    	cont "it already!"
+
+    	para "Show me its power,"
+    	line "use MEWTWO against"
+    	cont "me, now!"
+    	done
+
+_CeruleanCaveGiovanniLoseText::
+    	text "GIOVANNI: Even"
+    	line "MEWTWO couldn’t"
+    	cont "stop me?!"
+	cont "Pathetic..."
+    	done
+
+_CeruleanCaveGiovanniWinText::
+    	text "GIOVANNI: MEWTWO’s"
+    	line "strength... You’ve"
+    	cont "mastered it well!"
+	
+	para "The bond of a"
+	line "#MON and a"
+	cont "fierce TRAINER"
+    	done
+
+_CeruleanCaveGiovanniPostText::    
+    	text "GIOVANNI: MEWTWO"
+    	line "created an habitat"
+    	cont "for itself here"
+    	cont "in this cave"
+
+    	para "Its powers were"
+    	line "too dangerous"
+    	cont "for even itself"
+    	cont "to control."
+
+    	para "So it waited"
+    	line "for the strongest"
+    	cont "trainer to come."
+
+    	para "Now go,"
+    	line "look for the"
+    	cont "lead scientist"
+    	cont "of the MEWTWO"
+    	cont "project."
+    	done
+
+_CeruleanCaveGiovanniNoMewtwoText::
+    	text "GIOVANNI: I"
+    	line "will only battle"
+	cont "with MEWTWO"
+    	cont "by your side!"
+
+	para "I'll wait."
+    	done
+
+_CeruleanCaveGiovanniPostFadeText::
+    text "GIOVANNI: You had"
+    line "an amazing journey"
+    cont "<PLAYER>, you’re"
+    cont "strong enough to"
+    cont "face MEW. Go find"
+    cont "it. Use this."
+    done
+
+_ReceivedHiggsFossilText::
+    text "Received a"
+    line "HIGGS FOSSIL!@"
+
+_HiggsFossilNoRoomText::
+    text "No room for the"
+    line "HIGGS FOSSIL!@"
