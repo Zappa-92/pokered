a22324ff3ac5324342510298f3c92ca521693f2f
Alexis Bastiani <65498421+Zappa-92@users.noreply.github.com>
2026-03-20 17:35:59 -0300

    Update Route6Gate.asm

--- scripts/Route6Gate.asm
+++ scripts/Route6Gate.asm
@@ -9,66 +9,34 @@ Route6Gate_ScriptPointers:
 	dw Route6GateScript0
 	dw Route6GateScript1
 
-
 Route6GateScript0:
 	ld a, [wd728]
 	bit 6, a
-	ret nz                  ; Gate already open, exit
-    	ld hl, CoordsData_1e08c
-    	call ArePlayerCoordsInArray
-    	ret nc                  ; Wrong position, exit
-    	ld a, PLAYER_DIR_RIGHT
-    	ld [wPlayerMovingDirection], a
-    	xor a
-    	ld [hJoyHeld], a
-    	CheckEvent EVENT_GAVE_GUARD_DRINK  ; Track if drink was given
-    	jr nz, .drinkAlreadyGiven
-    	callba RemoveGuardDrink     ; Check for drink
-    	ld a, [$ffdb]
-    	and a
-    	jr z, .noDrink                  ; No drink, deny
-    	SetEvent EVENT_GAVE_GUARD_DRINK ; Mark drink as given
-    	ld a, [wObtainedBadges]
-    	bit 3, a
-    	jr z, .firstDrinkNoBadge        ; Drink given, no badge   Drink + Badge on first try
-	ld hl, wd728
-	set 6, [hl]                     ; Open gate
-	ld a, $3                        ; "Thanks!" + "Glug glug... you can go"
-	ld [hSpriteIndexOrTextID], a
-	jp DisplayTextID
-.noDrink:
-	ld a, $2                        ; "I'm thirsty... trouble ahead!"
+	ret nz
+	ld hl, CoordsData_1e08c
+	call ArePlayerCoordsInArray
+	ret nc
+	ld a, PLAYER_DIR_RIGHT
+	ld [wPlayerMovingDirection], a
+	xor a
+	ld [hJoyHeld], a
+	callba RemoveGuardDrink
+	ld a, [$ffdb]
+	and a
+	jr nz, .asm_1e080
+	ld a, $2
 	ld [hSpriteIndexOrTextID], a
 	call DisplayTextID
-	call Route6GateScript_1e0a1     ; Move guard
+	call Route6GateScript_1e0a1
 	ld a, $1
 	ld [wRoute6GateCurScript], a
 	ret
-.firstDrinkNoBadge:
-    	ld a, $5                        ; "Glug glug... Team Rocket!"
-    	ld [hSpriteIndexOrTextID], a
-    	call DisplayTextID
-    	call Route6GateScript_1e0a1
-    	ld a, $1
-    	ld [wRoute6GateCurScript], a
-    	ret
-.drinkAlreadyGiven:
-   	ld a, [wObtainedBadges]
-    	bit 3, a
-    	jr z, .noBadgeAfterDrink        ; Drink given, still no badge   Drink given previously, now has badge
-    	ld hl, wd728
-    	set 6, [hl]                     ; Open gate
-    	ld a, $4                        ; "Thanks for drinks... keep on!"
-    	ld [hSpriteIndexOrTextID], a    ; Skip "Glug glug" since already drank
-    	jp DisplayTextID
-.noBadgeAfterDrink:
-    	ld a, $6                        ; "Thanks, but Team Rocket..."
-    	ld [hSpriteIndexOrTextID], a
-    	call DisplayTextID
-    	call Route6GateScript_1e0a1
-    	ld a, $1
-    	ld [wRoute6GateCurScript], a
-    	ret
+.asm_1e080
+	ld hl, wd728
+	set 6, [hl]
+	ld a, $3
+	ld [hSpriteIndexOrTextID], a
+	jp DisplayTextID
 
 CoordsData_1e08c:
 	db $02,$03
@@ -97,9 +65,6 @@ Route6GateScript_1e0a1:
 	ret
 
 Route6Gate_TextPointers:
-	dw _SaffronGateText_1dfe7            ; ID 1 (unused in script, but kept for safety)
-	dw _SaffronGateText_1dfe7            ; ID 2 ("I'm thirsty... trouble ahead!")
-    	dw _SaffronGateText_8aaa9            ; ID 3 ("Thanks!" + "Glug glug...")
-    	dw _SaffronGateText_1dff6            ; ID 4 ("Thanks... keep on!")
-    	dw _SaffronGateText_FirstDrinkNoBadge ; ID 5 ("Glug glug... Team Rocket!")
-    	dw _SaffronGateText_NoBadgeAfterDrink ; ID 6 ("Thanks, but Team Rocket...")
+	dw Route6GateText1
+	dw Route6GateText2
+	dw Route6GateText3
