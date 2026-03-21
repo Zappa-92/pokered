4e9937f215bf9f0050e418493e9279cd089f1d07
Alexis Bastiani <65498421+Zappa-92@users.noreply.github.com>
2026-03-21 00:52:54 -0300

    Update CinnabarLabFossilRoom.asm

--- scripts/CinnabarLabFossilRoom.asm
+++ scripts/CinnabarLabFossilRoom.asm
@@ -4,7 +4,6 @@ CinnabarLabFossilRoom_Script:
 CinnabarLabFossilRoom_TextPointers:
 	dw Lab4Text1
 	dw Lab4Text2
-    dw BlaineLabText
 
 Lab4Script_GetFossilsInBag:
 ; construct a list of all fossils in the player's bag
@@ -107,115 +106,6 @@ Lab4Text2:
 	predef DoInGameTradeDialogue
 	jp TextScriptEnd
 
-BlaineLabText:
-    TX_ASM
-    CheckEvent EVENT_BEAT_GIOVANNI_CAVE_REMATCH
-    jp z, .done
-    CheckEvent EVENT_GAVE_DNA_TO_BLAINE
-    jr nz, .dnaReviving
-    CheckEvent EVENT_GAVE_HIGGS_TO_BLAINE
-    jr nz, .higgsReviving
-    ; Intro dialogue
-    ld hl, BlaineLabIntroText
-    call PrintText
-    ld a, HIGGS_FOSSIL
-    ld [wcf91], a
-    call IsItemInBag
-    jr z, .done           ; Changed to z (not found)
-    ld hl, BlaineLabHiggsText
-    call PrintText
-    call BlaineLabScript_RemoveItem
-    SetEvent EVENT_GAVE_HIGGS_TO_BLAINE
-    ld a, 240  ; 4x normal fossil steps
-    ld [wWalkCounter], a
-    SetEvent EVENT_HIGGS_STILL_REVIVING
-    jr .done
-.higgsReviving
-    CheckEvent EVENT_HIGGS_STILL_REVIVING
-    jr nz, .wait
-    ld hl, BlaineLabHiggsFailedText
-    call PrintText
-    ; Check DNA Codes after Higgs fails
-    ld a, DNA_CODES
-    ld [wcf91], a
-    call IsItemInBag
-    jr z, .done           ; Changed to z (not found)
-    ld hl, BlaineLabDNAText
-    call PrintText
-    call BlaineLabScript_RemoveItem
-    SetEvent EVENT_GAVE_DNA_TO_BLAINE
-    ld a, 240  ; 4x steps
-    ld [wWalkCounter], a
-    SetEvent EVENT_DNA_STILL_REVIVING
-    jr .done
-.wait
-    ld hl, BlaineLabWaitText
-    call PrintText
-    jr .done
-.dnaReviving
-    CheckEvent EVENT_DNA_STILL_REVIVING
-    jr nz, .wait
-    ld hl, BlaineLabMewText
-    call PrintText
-    lb bc, MEW, 30  ; Level 30 Mew
-    call GivePokemon
-    jr nc, .done
-    ld hl, BlainePostMewText
-    call PrintText
-    ResetEvents EVENT_GAVE_DNA_TO_BLAINE, EVENT_DNA_STILL_REVIVING
-.done
-    jp TextScriptEnd
-
-BlaineLabScript_RemoveItem:
-    ld hl, wBagItems
-    ld bc, $0000
-.loop
-    ld a, [hli]
-    cp $ff
-    ret z
-    ld b, a             ; Store item ID from bag
-    ld a, [wcf91]       ; Load target item ID
-    cp b                ; Compare with bag item
-    jr z, .foundItem
-    inc hl
-    inc c
-    jr .loop
-.foundItem
-    ld hl, wNumBagItems
-    ld a, c
-    ld [wWhichPokemon], a
-    ld a, 1
-    ld [wItemQuantity], a
-    jp RemoveItemFromInventory
-
 LoadFossilItemAndMonNameBank1D:
 	jpba LoadFossilItemAndMonName
 
-; Text Pointers
-BlaineLabIntroText:
-    TX_FAR _BlaineLabIntroText
-    db "@"
-
-BlaineLabHiggsText:
-    TX_FAR _BlaineLabHiggsText
-    db "@"
-
-BlaineLabHiggsFailedText:
-    TX_FAR _BlaineLabHiggsFailedText
-    db "@"
-
-BlaineLabDNAText:
-    TX_FAR _BlaineLabDNAText
-    db "@"
-
-BlaineLabWaitText:
-    TX_FAR _BlaineLabWaitText
-    db "@"
-
-BlaineLabMewText:
-    TX_FAR _BlaineLabMewText
-    db "@"
-
-BlainePostMewText:
-    TX_FAR _BlainePostMewText
-    db "@"
