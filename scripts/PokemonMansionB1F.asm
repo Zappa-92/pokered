e40d568345fda793f5028f27ec5a42e52c6595a8
Alexis Bastiani <65498421+Zappa-92@users.noreply.github.com>
2026-03-23 15:53:20 -0300

    Update PokemonMansionB1F.asm

--- scripts/PokemonMansionB1F.asm
+++ scripts/PokemonMansionB1F.asm
@@ -140,9 +140,9 @@ PokemonMansionB1F_TextPointers:
 	dw PickUpItemText
 	dw Mansion4Text7
 	dw PickUpItemText
+	dw Mansion3Text6
 	dw MansionB1FMewShrineText
 	dw PickUpItemText  ; DNA Codes
-	dw Mansion3Text6
 
 Mansion4TrainerHeader0:
 	dbEventFlagBit EVENT_BEAT_MANSION_4_TRAINER_0
