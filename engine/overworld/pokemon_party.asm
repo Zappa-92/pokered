633daa821240a47750b638a757a2c7c190e90e4b
Alexis Bastiani <65498421+Zappa-92@users.noreply.github.com>
2026-04-02 13:53:09 -0300

    Refactor IsPokemonInParty function logic

--- engine/overworld/pokemon_party.asm
+++ engine/overworld/pokemon_party.asm
@@ -5,23 +5,34 @@ IsPokemonInParty:
 ; Input: a = Pokémon ID
 ; Output: Z = 1 → NO está
 ;         Z = 0 → SÍ está
+
     push bc
     push hl
-    ld b, a              ; guardar Pokémon buscado
+
+    ld b, a                  ; Pokémon buscado
+    ld a, [wPartyCount]
+    and a
+    jr z, .not_found         ; party vacía
+
+    ld c, a                  ; cantidad en C
     ld hl, wPartySpecies
-    inc hl               ; SALTEAR el count
+
 .loop
     ld a, [hli]
-    cp $FF               ; fin de lista
-    jr z, .not_found
     cp b
     jr z, .found
-    jr .loop
-.found
-    or a                 ; Z = 0
-    jr .done
+
+    dec c
+    jr nz, .loop
+
 .not_found
-    xor a                ; Z = 1
+    xor a                    ; Z = 1
+    jr .done
+
+.found
+    ld a, 1
+    or a                     ; Z = 0
+
 .done
     pop hl
     pop bc
